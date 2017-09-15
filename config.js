f133f11bcdb9dab58e0def027386f3689a4a90e5
Admin <ekkawit.lertkrai@gmail.com>
2017-09-15 10:31:43 +0700

    Fixed Bug

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const API_SERVER = 'http://174.138.31.29:3000';
-export const CID = '2129700003006';
+export const CID = '3300900307881';
 
 // export const API_SERVER = 'http://localhost:3000';
 // export const CID = '1120300068864';
